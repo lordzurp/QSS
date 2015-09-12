a30eadbc14575c3cf1d2954120bb54df523749f2
Ti nicO <zurp.online@gmail.com>
2015-09-12 18:51:15 +0200

    minor changes

--- index.php
+++ index.php
@@ -33,8 +33,7 @@
 				var date = new Date();
 				if ( date.getHours() <= 9 ) {var heure = '0'+date.getHours();} else {var heure = date.getHours();}
 				if ( date.getMinutes() <= 9 ) {var minutes = '0'+date.getMinutes();} else {var minutes = date.getMinutes();}
-				if ( date.getSeconds() <= 9 ) {var secondes = '0'+date.getSeconds();} else {var secondes = date.getSeconds();}
-				text = '<h4>'+heure+':'+minutes+':'+secondes+'</h4>';
+				text = '<h4>'+heure+':'+minutes+'</h4>';
 				document.getElementById('time').innerHTML = text;
 			}
 			function refresh()
@@ -42,7 +41,7 @@
 				time();
 				show();
 				$( "#status" ).load( "check.php" , hide() );
-				setTimeout(refresh, 5000);
+				setTimeout(refresh, 30000);
 			}
 			function show() { document.getElementById('reload').style.visibility='visible'; }
 			function hide() { document.getElementById('reload').style.visibility='hidden'; }
@@ -61,8 +60,6 @@
 		<script type="text/javascript">
 			refresh();
 		</script>
-		
-		<a href="https://github.com/lordzurp/QSS" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>
 		
 		<footer class="footer">
 			<div class="container">
